96b59770f1b7957bec47163db7ed83ee553cdc64
DennisMaas <mail@dennismaas.de>
2021-03-31 11:17:12 +0200

    Initial commit

---
+++ src/main/java/de/dennismaas/osbdemo/OsbDemoApplication.java
@@ -0,0 +1,13 @@
+package de.dennismaas.osbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsbDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dennismaas/osbdemo/OsbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dennismaas.osbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsbDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
