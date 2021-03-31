932d3cfa1052648434c82cabaa129b41974624a1
DennisMaas <mail@dennismaas.de>
2021-03-31 13:29:55 +0200

    add RestTemplateConfiguration

---
+++ src/main/java/de/dennismaas/osbdemo/web/RestTemplateConfiguration.java
@@ -0,0 +1,14 @@
+package de.dennismaas.osbdemo.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    public RestTemplate template(){
+        return new RestTemplate();
+    }
+}
