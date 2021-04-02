18cbdbb8d6afa51379de4a2f3b815a142c7f2592
DennisMaas <mail@dennismaas.de>
2021-04-02 13:31:26 +0200

    remove unused annotations from Place model

--- src/main/java/de/dennismaas/osbdemo/web/model/Place.java
+++ src/main/java/de/dennismaas/osbdemo/web/model/Place.java
@@ -4,17 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "places")
 public class Place {
 
-        @Id
         private String id;
         private String placeDescription;
 
